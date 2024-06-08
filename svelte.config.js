19abf3b95f7251f44c6b25b03e998c8cf5f46d64
Abir Muhtasim <1905066@ugrad.cse.buet.ac.bd>
2024-06-09 03:28:30 +0600

    changed adapter to adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-cloudflare';
+// import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 import { mdsvex } from 'mdsvex';
